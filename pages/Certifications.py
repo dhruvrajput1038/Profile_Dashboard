21305c9c5a8c877c01dc2cf696f397d9039c9300
Dhruv Rajput <138105309+dhruvrajput1038@users.noreply.github.com>
2024-03-14 16:10:01 +0530

    Update Certifications.py

--- pages/Certifications.py
+++ pages/Certifications.py
@@ -10,12 +10,26 @@ st.write('\n')
 
 # Cert. 2
 st.write('2. Crash Course on Python')
-st.write('Certificate Link : -------------')
+st.write('Certificate Link : https://drive.google.com/file/d/1weqKjfzxPcNQGHY4IDa_BxOVo6IWXV-T/view?usp=sharing')
 st.caption('Demonstrated strong understanding of object-oriented programming concepts in Python. ')
 st.write('\n')
 
 # Cert. 3
 st.write('3. Machine Learning Course by WIPRO')
-st.write('Certificate Link : -------------')
+st.write('Certificate Link : https://drive.google.com/file/d/1Oru_GlITS4-Ty9SCnpEJFhuVWrbQs6IK/view?usp=sharing')
 st.caption('Successfully trained and deployed machine learning models to solve real world problems. ')
+st.write('\n')
+
+# Cert. 4
+st.write('3. WIPRO FUTURE SKILLS')
+st.write('Certificate Link : https://drive.google.com/file/d/1X0y35pKSGavdqRNwkcC8UPX_atnvkW3Q/view?usp=sharing')
+#st.caption('Successfully trained and deployed machine learning models to solve real world problems. ')
+st.write('\n')
+
+
+# Cert. 4
+st.write('3. AWS Academy Cloud Foundations')
+st.write('Certificate Link : https://drive.google.com/file/d/1o-xhuqADdXvYKHo9wv3K_7sQKL-DW2py/view?usp=sharing')
+st.write('Badge Link : https://drive.google.com/file/d/15n1S0a59OJCVVPQKialGUQynJjxBIrgv/view?usp=sharing')
+#st.caption('Successfully trained and deployed machine learning models to solve real world problems. ')
 st.write('\n')
